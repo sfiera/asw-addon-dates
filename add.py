3e3c913f61986f3b655457a2e94c04da95526689
Chris Pickel <sfiera@twotaled.com>
2020-05-26 08:55:27 +0900

    Add script to download archives

---
+++ add.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import requests
+import sys
+import urllib.parse
+
+
+DATE = re.compile(r"https://web.archive.org/web/(\d{8})\d{6}id_/.*")
+# /http://www.ambrosiasw.com/cgi-bin/vftp/show.pl?product=avara&category=*&display=date&page=1
+
+exit = 0
+for url in sys.argv[1:]:
+    m = DATE.match(url)
+    if m is None:
+        exit = 1
+        print("not a good URL: %s" % url)
+        continue
+    q = urllib.parse.parse_qs(urllib.parse.urlparse(url).query)
+    date = m.group(1)
+    game, = q["product"]
+    page, = q["page"]
+
+    r = requests.get(url)
+    r.raise_for_status()
+    try:
+        os.makedirs(f"archive/{game}")
+    except FileExistsError:
+        pass
+    with open(f"archive/{game}/{date}-{page}.json", "wb") as f:
+        f.write(r.content)
+sys.exit(exit)
