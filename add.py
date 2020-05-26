41091aba020bd305b35fef5b6264e45a8c3fb356
Chris Pickel <sfiera@twotaled.com>
2020-05-26 09:09:19 +0900

    Insert id_ if missing

--- add.py
+++ add.py
@@ -7,7 +7,7 @@ import sys
 import urllib.parse
 
 
-DATE = re.compile(r"https://web.archive.org/web/(\d{8})\d{6}id_/.*")
+DATE = re.compile(r"(https://web.archive.org/web/(\d{8})\d{6})(?:id_)?/(.*)")
 # /http://www.ambrosiasw.com/cgi-bin/vftp/show.pl?product=avara&category=*&display=date&page=1
 
 exit = 0
@@ -18,9 +18,11 @@ for url in sys.argv[1:]:
         print("not a good URL: %s" % url)
         continue
     q = urllib.parse.parse_qs(urllib.parse.urlparse(url).query)
-    date = m.group(1)
+    prefix, date, suffix = m.groups()
     game, = q["product"]
     page, = q["page"]
+    url = f"{prefix}id_/{suffix}"
+    print(url)
 
     r = requests.get(url)
     r.raise_for_status()
